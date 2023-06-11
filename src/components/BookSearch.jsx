784b5f05e1a590fe7eb87731bec345d8a24e6e82
ilyas182 <ilyas2104@gmail.com>
2023-06-11 13:22:45 +0800

    display booksearch results on screen

--- src/components/BookSearch.jsx
+++ src/components/BookSearch.jsx
@@ -4,14 +4,17 @@ import Book from "./Book";
 export default function BookSearch(){
 
    const location = useLocation();
-   const {from} = location.state;
-   console.log('results', location.state);
+   const {searchResults} = location.state;
+   console.log('results', searchResults);
     
     const navigate = useNavigate();
     return (
     <>
     <div>
     <button onClick={() => navigate(-1)}>Back</button>
+    {searchResults.map(({key, title}) => 
+        <p>`${key}:{title}`</p>
+    )}
     <Link to="/book"><h2>Book </h2></Link>
     <h2>Book </h2>
     <h2>Book </h2>
